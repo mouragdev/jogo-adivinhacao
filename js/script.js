9afcc78a1917296d20f7433b89bae7857a0555ac
Gabriel Moura <gmoura.dev@outlook.com>
2026-07-22 11:43:16 -0300

    Estrutura HTML + Organização Algoritmo

---
+++ js/script.js
@@ -0,0 +1,14 @@
+/*
+
+    1. Gerar um número aleatório entre 1 e 100 e armazená-lo em uma variável.
+    2. Definir o número máximo de tentativas (ex: 10).
+    3. Inicializar o contador de tentativas.
+    4. Capturar o valor inserido no input (palpite do jogador).
+    5. Validar se o palpite é um número válido entre 1 e 100.
+    6. Comparar o palpite com o número secreto e exibir uma mensagem:
+    7. Decrementar o contador de tentativas.
+    8. Exibir o número de tentativas restantes.
+    9. Se o jogador atingir o número máximo de tentativas, o jogo termina com a mensagem "Você perdeu! O número secreto era X".
+
+
+*/ 
